ff4e6c98b17500a7b9aa128b8306ed6c1916ef6c
wangshiyuan <wangshiyuan@zhito.com>
2025-04-22 16:32:30 +0800

    init

---
+++ src/test/java/com/shiyuan/base/DemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiyuan.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
